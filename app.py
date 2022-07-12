49fa3ea227788c152d70232c86e33b15c5dc8cfd
ArooKrishna <i.arvindkrishna@gmail.com>
2022-07-12 22:16:14 +0530

    idk man, did some shit

--- app.py
+++ app.py
@@ -1,43 +1,20 @@
 import base64
+from calendar import c
 from io import BytesIO
-import random
 import string
 from PIL import Image, ImageFont, ImageDraw
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
-
-def createMeme(captions):
-    captions = convertCaptionsCamelCase(captions)
-    try:
-        if(captions['top'] == captions['bottom'] == ''):
-            raise ValueError
-        filename = generateImage(captions)
-    except ValueError as e:
-        print(f"Bad request: {str(e)}")
-        filename = handleErrorMeme()
-
-    return filename
+CORS(app) # allow cross-origin requests
 
 
-def handleErrorMeme():
-    caption_choices = [
-        {'top': "i know how to", 'bottom': "make memes"},
-        {'top': '', 'bottom': "i can make memes bro"},
-        {'top': "i am a professional", 'bottom': "meme maker"},
-        {'top': "my meme game", 'bottom': "is strong"},
-        {'top': "i know how to", 'bottom': "use saas"}
-    ]
-    captions = random.choice(caption_choices)
-    filename = createMeme(captions)
-    return filename
-
-
-def convertCaptionsCamelCase(captions):
-    print(captions)
-    # captions = captions
+def splitCaptionsIntoParts(captions):
+    """
+        Split captions into parts, so that they can be displayed in multiple lines.
+        Returns a list of lists, where each list contains the captions for one line.
+    """
     top = captions[:len(captions)//2]
     bottom = captions[len(captions)//2:]
     bottom = bottom.split(" ")
@@ -45,20 +22,8 @@ def convertCaptionsCamelCase(captions):
     bottom = " ".join(bottom[1:])
     captions = [top, bottom]
     n_words = [len(caption.split()) for caption in captions]
-    text = " ".join(captions)
-    new_text = str()
-    flag = False
-    for ch in text:
-        if ch in string.ascii_letters:
-            if flag:
-                new_text += ch
-                flag = False
-            else:
-                new_text += ch
-                flag = True
-        else:
-            new_text += ch
-
+    new_text = " ".join(captions)
+    
     new_text = new_text.split()
     new_captions = list()
     pos = 0
@@ -77,46 +42,29 @@ def index():
 
 @app.route('/gen', methods=['GET', 'POST'])
 def generateImage():
-    print(request.method)
+    if request.method == 'POST': 
+        # ! idk, for some reason even with CORS enabled, GET requests are not allowed
 
-
-    if 1 == 1: 
         # read body of request
         content = request.get_json()
-        print(content)
-        # return "jjj"
         captions = content['caption']
-        captions = convertCaptionsCamelCase(captions)
-        print(captions)
-        # return "kkkk"
+        captions = splitCaptionsIntoParts(captions)
+
         img_base64 = content['img_base64']
         img_base64 = img_base64.split(",")[1]
         # create image from base64
-        img = Image.open(BytesIO(base64.b64decode(img_base64)))
-        # render the image with a popup
-        img.save('img/meme.png')
         
+        img = Image.open(BytesIO(base64.b64decode(img_base64)))
+        # add text to image
         for position, caption in list(captions.items()):
             addText(img, position, caption)
 
         # render the image on the browser
+        # So save the image to a buffer, and send it back as a base64 string
         img.save('img/meme.png')
         img_base64 = base64.b64encode(open('img/meme.png', 'rb').read())
         img_base64 = img_base64.decode('utf-8')
         return jsonify({"img_base64": img_base64})
-
-        # captions = [c for c in captions.values() if c.strip() != ""]
-        # filename = "-".join(captions)+".jpg"
-        # filename = filename.replace(' ', '-')
-        # img.save("img/final.png")
-        # # img.close()
-        # # return filename
-        # # encode image to base64
-        # img_base64 = base64.b64encode(open("img/final.png", "rb").read())
-        # img_base64 = img_base64.decode("utf-8")
-        # print(img_base64)
-        # # return jsonify({"img_base64": img_base64})
-        # return render_template('index.html', img_base64=img)
 
 
 def addText(img, pos, msg):
